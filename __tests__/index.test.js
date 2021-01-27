abe6f70c7e606ff6aea068c9ad70952c228a8b13
jameskip <me@jameskip.com>
2021-01-27 11:34:02 -0600

    First commit

---
+++ __tests__/index.test.js
@@ -0,0 +1,11 @@
+describe("Google", () => {
+  beforeAll(async () => {
+    await page.goto("https://vaccine.heb.com/scheduler");
+  });
+
+  it("shoud be out of appointments", async () => {
+    await expect(page).toMatch(
+      "There are no appointments available, but we add new spots as we receive more vaccines. Please check back for updates."
+    );
+  });
+});
